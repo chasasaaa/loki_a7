c2fe2263d0ca2c8c25fc20651a6f1daeb0abf234
bobager03 <89834354+bobager03@users.noreply.github.com>
2022-03-24 17:08:01 +0700

    Update pertemuan_mingguan.js

--- pertemuan_mingguan.js
+++ pertemuan_mingguan.js
@@ -4,11 +4,41 @@ const app = express()
 
 //Pertemuan Mingguan RPS
 app.post("/dosen/tambah/pertemuan", function(req,res){
+    let pert = {
+        "message": "Pertemuan Mingguan RPS berhasil ditambahkan",
+        "code_error": 0,
+        "mata kuliah": {
+            "kode": "JSI62125",
+            "nama mata kuliah": "Pemograman Web",
+            "jumlah pertemuan": "1 pertemuan"
+        }
+    }
+    res.send(pert)
     res.send("Pertemuan Mingguan RPS berhasil ditambahkan");
 })
 app.put("/dosen/update/pertemuan", function(req,res){
+    let pert = {
+        "message": "Pertemuan Mingguan RPS berhasil di update",
+        "code_error": 0,
+        "mata kuliah": {
+            "kode": "JSI62125",
+            "nama mata kuliah": "Pemograman Web",
+            "jumlah pertemuan": "2 pertemuan"
+        }
+    }
+    res.send(pert)
     res.send("Pertemuan Mingguan RPS berhasil di update");
 })
 app.delete("/dosen/delete/pertemuan", function(req,res){
+    let pert = {
+        "message": "Pertemuan Mingguan RPS berhasil dihapus",
+        "code_error": 0,
+        "mata kuliah": {
+            "kode": "JSI62125",
+            "nama mata kuliah": "Pemograman Web",
+            "jumlah pertemuan": "2 pertemuan"
+        }
+    }
+    res.send(pert)
     res.send("Pertemuan Mingguan RPS berhasil dihapus");
 })
